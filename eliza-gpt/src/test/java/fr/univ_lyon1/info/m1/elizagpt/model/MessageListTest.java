378fec6bf498a8143ef354b8d8eeff30b8faf8c9
Desbiaux Arthur <arthur.desbiaux@etu.univ-lyon1.fr>
2023-12-10 12:39:50 +0100

    MessageList test 100% coverage

---
+++ eliza-gpt/src/test/java/fr/univ_lyon1/info/m1/elizagpt/model/MessageListTest.java
@@ -0,0 +1,51 @@
+package fr.univ_lyon1.info.m1.elizagpt.model;
+
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.List;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Tests for MessageList.
+ */
+public class MessageListTest {
+    private MessageList messageList = new MessageList();
+
+    @Test
+    void testMessageListConstructor() {
+        assertThat(messageList.pullLastMessage().getMessage(), is("Bonjour"));
+        assertThat(messageList.pullLastMessage().getIsFromEliza(), is(true));
+    }
+
+    @Test
+    void testMessageListConstructorCopy() {
+        messageList.add("Salut mec", false);
+        messageList.add("Pas de réponse", true);
+        MessageList messageListTest = new MessageList(messageList);
+
+        assertThat(messageListTest.getSize(), is(3));
+    }
+
+    @Test
+    void testMessageListPull() {
+        MessageList messageList = new MessageList();
+        messageList.add("Message 1", true);
+        messageList.add("Message 2", false);
+        messageList.add("Message 3", true);
+
+        List<Message> allMessages = messageList.pullAllMessage();
+
+        assertThat(allMessages.size(), is(4));
+
+        assertThat(allMessages.get(0).getMessage(), is("Bonjour"));
+        assertThat(allMessages.get(1).getMessage(), is("Message 1"));
+        assertThat(allMessages.get(2).getMessage(), is("Message 2"));
+        assertThat(allMessages.get(3).getMessage(), is("Message 3"));
+    }
+
+
+
+
+
+}
